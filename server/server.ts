34716b7a48d33490fe4434a75ae7641185ce2a3a
Mattholee <matthew0505@gmail.com>
2023-05-08 14:21:09 -0400

    will create boilerplate using vite and create skeleton server.ts

---
+++ server/server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+// import userRouter from './routes/userRouter';
+import path from 'path';
+
+const app = express();
+const PORT = 6666;
+
+// Add body parser
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../index')));
+
+// Handle invalid endpoint
+app.use((req: express.Request, res: express.Response) => {
+  res.status(404).send('Not Found');
+});
+
+//creates type for error handler
+type ErrHndl = {
+  log: string;
+  status: number;
+  message: {
+    err: string;
+  };
+};
+
+// Handle errors
+app.use((err: ErrHndl, req: Request, res: Response) => {
+  const defaultErr: ErrHndl = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj: ErrHndl = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
