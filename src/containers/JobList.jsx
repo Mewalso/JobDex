224a89ac817e6cd1282374fb68ced53c09bd1740
Ty Harmon <tyharmon.wa@gmail.com>
2023-05-10 11:33:02 -0700

    front end jobDetailView build out

--- src/containers/JobList.jsx
+++ src/containers/JobList.jsx
@@ -10,6 +10,37 @@ const JobList = () => {
 
   const jobsArray = [];
 
+
+  
+
+  function newJob (){
+  //  get req to DB for empty job data (id should be the only non-null)
+  //    we could also send the null obj if necessary
+  fetch('http://localhost:4000/createJobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+            company: null,
+            position: null,
+            link: null,
+            app_contact: null,
+            cover_letter: null,
+            status: null,
+            date_submitted: null,
+            dd: false,
+            dd_name: null,
+            dd_message: null,
+            dd_contact_info: null,
+            dd_follow_up: false,
+            dd_follow_up_date: null
+      }),
+    })
+    .then((res) => res.json())
+    .then(window.location.reload());     
+  }
+  
   // iterate through job state and put a Job <job/> component into an array for each applciation in state
 
   for(let i = 0; i < jobs.length; i++) {
@@ -20,7 +51,7 @@ const JobList = () => {
     <div>
       <div className='topbar'>
         <h1>Job List</h1>
-        <button>create new job</button>
+        <button onClick={() => newJob()}>Create New Job</button>
       </div>
       <div className='jobs-array'>{jobsArray}</div>
     </div>
